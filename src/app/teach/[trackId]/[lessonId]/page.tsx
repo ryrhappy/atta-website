1af7be14d6fafaf653e16911fd555da23489e286
ryrhappy <ryr18295718374@sina.com>
2026-04-27 10:34:21 +0800

    feat: static export + Cloudflare Pages auto deploy

--- src/app/teach/[trackId]/[lessonId]/page.tsx
+++ src/app/teach/[trackId]/[lessonId]/page.tsx
@@ -1,4 +1,14 @@
+import { tracks } from "@/app/lib/courses";
 import LessonPageClient from "./LessonPageClient";
+
+export function generateStaticParams() {
+  return tracks.flatMap((track) =>
+    track.lessons.map((lesson) => ({
+      trackId: track.id,
+      lessonId: lesson.id,
+    }))
+  );
+}
 
 export default function LessonPage() {
   return <LessonPageClient />;
--- src/app/teach/[trackId]/page.tsx
+++ src/app/teach/[trackId]/page.tsx
@@ -1,4 +1,9 @@
+import { tracks } from "@/app/lib/courses";
 import TrackPageClient from "./TrackPageClient";
+
+export function generateStaticParams() {
+  return tracks.map((track) => ({ trackId: track.id }));
+}
 
 export default function TrackPage() {
   return <TrackPageClient />;
